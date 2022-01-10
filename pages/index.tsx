a1c6ad132aa6bef5d31ab595595ca1e20e671f7d
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-01-11 01:11:46 +0700

    feat: add UI for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import SocialIcon from 'components/SocialIcon';
 import Image from 'next/image';
 import { parseISO, format } from 'date-fns';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import MobileMenu from 'components/MobileMenu';
+import { useState } from 'react';
+import Sidebar from 'components/Sidebar';
 
 interface Post {
   title: string;
@@ -10,53 +14,39 @@ interface Post {
 }
 
 export default function HomePage({ posts }: { posts: Post[] }) {
-  return (
-    <div className="bg-gray-50 dark:bg-zinc-800 text-black dark:text-white">
-      <div className="flex mx-auto p-10 max-w-4xl">
-        {/* Sidebar */}
-        <div className="md:flex flex-col self-start sticky top-10 pr-10 hidden">
-          <Image
-            className="shrink-0 rounded-full"
-            src="/photo.gif"
-            alt="avatar"
-            layout="fixed"
-            width={75}
-            height={75}
-          />
-          <h1 className="font-bold text-lg py-4">Hung.Dev</h1>
-          <p className="pb-4 text-gray-600 dark:text-gray-400">
-            Software Engineer. Amateur Triathlete
-          </p>
+  const [isOpen, setIsOpen] = useState(false);
 
-          <nav>
-            <ul className="flex flex-col gap-3 py-4">
-              <li>Blogs</li>
-              <li>About me</li>
-              <li>Contact me</li>
-              <li>Wordpress Blog</li>
-            </ul>
-          </nav>
-          <ul className="flex gap-2 py-4">
-            <SocialIcon name="twitter" url="https://www.twitter.com/hung_dev" />
-            <SocialIcon name="github" url="https://github.com/nvh95" />
-            <SocialIcon
-              name="linkedin"
-              url="https://www.linkedin.com/in/nvh95"
-            />
-            <SocialIcon name="facebook" url="https://www.facebook.com/nvh95" />
-          </ul>
+  return (
+    <div className="min-h-full bg-gray-50 dark:bg-zinc-800 text-black dark:text-white">
+      <div className="mx-auto p-5 md:p-10 max-w-4xl">
+        {/* Header */}
+        <div>
+          <button
+            className="p-5 mb-5 md:hidden px-5 -mx-5"
+            onClick={() => setIsOpen(true)}
+          >
+            <HamburgerMenuIcon />
+          </button>
+          <MobileMenu
+            open={isOpen}
+            onOpenChange={(open: boolean) => setIsOpen(open)}
+          />
         </div>
-        {/* Blog Posts */}
-        <div className="flex flex-col">
-          {posts.map((post) => (
-            <div key={post.slug} className="flex flex-col pb-6">
-              <time className="text-sm uppercase">
-                {format(parseISO(post.created), 'MMMM dd, yyyy')}
-              </time>
-              <h2 className="font-medium text-xl pb-1">{post.title}</h2>
-              <p className="dark:text-gray-50">{post.description}</p>
-            </div>
-          ))}
+        <div className="flex">
+          {/* Sidebar */}
+          <Sidebar />
+          {/* Blog Posts */}
+          <div className="flex flex-col">
+            {posts.map((post) => (
+              <div key={post.slug} className="flex flex-col pb-6">
+                <time className="text-sm uppercase">
+                  {format(parseISO(post.created), 'MMMM dd, yyyy')}
+                </time>
+                <h2 className="font-medium text-xl pb-1">{post.title}</h2>
+                <p className="dark:text-gray-50">{post.description}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
